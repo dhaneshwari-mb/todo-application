aa9cb7dfbe0f2d3d042a5e48a64c4976e14e0e00
dhaneshwari-mb <hey.dhaneshwari@gmail.com>
2025-08-06 11:08:23 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: 'Roboto',
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
